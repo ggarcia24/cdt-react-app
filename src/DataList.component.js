5e7c5327edf4d48325f089d7e9cb2b35b413cccc
Gonzalo García <gonzalogarcia243@gmail.com>
2021-11-24 15:36:39 +0100

    Update code to use a demo ToDo App
    Taken from https://blog.logrocket.com/set-up-graphql-api-using-serverless-react/

---
+++ src/DataList.component.js
@@ -0,0 +1,191 @@
+import React from "react";
+import styled from "styled-components";
+import { List, Checkbox, Input, Button, Popconfirm, message } from "antd";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+/** App theme */
+// import Colors from "../../theme/colors";
+
+/** GraphQL Queries */
+const UPDATE_TODO = gql`
+  mutation UpdateToDo($input: ToDoUpdateInput) {
+    updateTodo(input: $input) {
+      id
+      createdAt
+      description
+      updatedAt
+      dueDate
+      completed
+    }
+  }
+`;
+const CREATE_TODO = gql`
+  mutation createTodo($input: ToDoCreateInput!) {
+    createTodo(input: $input) {
+      id
+      createdAt
+      updatedAt
+      completed
+      description
+      dueDate
+    }
+  }
+`;
+const DELETE_TODO = gql`
+  mutation DeleteToDo($id: ID!) {
+    deleteTodo(id: $id) {
+      id
+      createdAt
+      description
+      updatedAt
+      dueDate
+      completed
+    }
+  }
+`;
+const LIST_TODOS = gql`
+  query ListTodos {
+    listToDos {
+      id
+      createdAt
+      description
+      updatedAt
+      dueDate
+      completed
+    }
+  }
+`;
+
+const DataList = (props) => {
+  const [description, updateDescription] = React.useState("");
+  const [updateToDoMutation] = useMutation(UPDATE_TODO);
+  const [createToDoMutation] = useMutation(CREATE_TODO);
+  const [deleteToDoMutation] = useMutation(DELETE_TODO);
+  //   const { data } = props;
+
+  const { loading, error, data } = useQuery(LIST_TODOS);
+
+  console.log(data);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  function handleCheck(event, item) {
+    event.preventDefault();
+    const completed =
+      typeof item.completed === "boolean" ? !item.completed : true;
+
+    updateToDoMutation({
+      variables: { input: { completed, id: item.id } },
+      refetchQueries: [
+        {
+          query: LIST_TODOS,
+        },
+      ],
+    })
+      .then((res) => message.success("Item updated successfully"))
+      .catch((err) => {
+        message.error("Error occurred while updating item");
+        console.log(err);
+      });
+  }
+
+  function handleSubmit(event, item) {
+    event.preventDefault();
+    createToDoMutation({
+      variables: { input: { description, dueDate: "2022-01-02T00:01:00Z" } },
+      refetchQueries: [
+        {
+          query: LIST_TODOS,
+        },
+      ],
+    })
+      .then((res) => message.success("Item created successfully"))
+      .catch((err) => {
+        message.error("Error occurred while creating item");
+        console.log(err);
+      });
+  }
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      // user pressed enter
+      createToDoMutation({
+        variables: { input: { description, dueDate: "2022-01-02T00:01:00Z" } },
+        refetchQueries: [
+          {
+            query: LIST_TODOS,
+          },
+        ],
+      })
+        .then((res) => {
+          message.success("Item created successfully");
+        })
+        .catch((err) => {
+          message.error("Error occurred while creating item");
+          console.log(err);
+        });
+    }
+  }
+
+  function handleDelete(event, item) {
+    deleteToDoMutation({
+      variables: { id: item.id },
+      refetchQueries: [
+        {
+          query: LIST_TODOS,
+        },
+      ],
+    })
+      .then((res) => {
+        message.success("Deleted successfully");
+      })
+      .catch((err) => {
+        message.error("Error occurred while deleting item");
+        console.log(err);
+      });
+  }
+
+  return (
+    <>
+      <List
+        header={
+          <div style={{ display: "flex" }}>
+            <Input
+              placeholder="Enter todo name"
+              value={description}
+              onChange={(event) => updateDescription(event.target.value)}
+              style={{ marginRight: "10px" }}
+              onKeyDown={handleKeyPress}
+            />
+            <Button name="add" onClick={handleSubmit}>
+              add
+            </Button>
+          </div>
+        }
+        bordered
+        dataSource={data.listToDos}
+        renderItem={(item) => (
+          <List.Item>
+            <Checkbox
+              checked={item.completed}
+              onChange={(event) => handleCheck(event, item)}
+            >
+              {item.description}
+            </Checkbox>
+            <Popconfirm
+              title="Are you sure to delete this item?"
+              onConfirm={(event) => handleDelete(event, item)}
+              okText="Yes"
+              cancelText="No"
+            >
+              <a href="#">Delete</a>
+            </Popconfirm>
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
+
+export default DataList;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createAuthLink } from "aws-appsync-auth-link";
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
+// import { AUTH_TYPE } from "aws-appsync";
+import {
+  ApolloProvider,
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+} from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
+
+/** Ant design */
+import "antd/dist/antd.css";
+
+/** App entry */
+import App from "./App";
+
+/** AWS config */
+import AppSyncConfig from "./aws-exports";
+
+const config = {
+  url: AppSyncConfig.GraphQlApiUrl,
+  region: process.env.REACT_APP_REGION,
+  auth: {
+    type: "API_KEY",
+    apiKey: AppSyncConfig.GraphQlApiKeyDefault,
+  },
+};
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    createAuthLink(config),
+    createSubscriptionHandshakeLink(config),
+  ]),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
+    },
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
